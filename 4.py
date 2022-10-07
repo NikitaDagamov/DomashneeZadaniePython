2e0924927f0a52363549d08995892883a2c7d2ef
Nic <nicitaadagamov@yandex.ru>
2022-10-07 21:36:36 +0300

    .

---
+++ 4.py
@@ -0,0 +1,6 @@
+years = int(input("Введите кол-во лет: "))
+NumExhibits = 35040 * years
+print("За", years, "лет вы успеете посмотреть только", NumExhibits, "экспонатов из 2.8 миллионов")
+Exhibits = int(input("Введите кол-во экспонатов: "))
+NumExhibits = 5 * Exhibits
+print("Чтобы посмотреть", Exhibits, "экспонатов вам потребуется", NumExhibits, "минут")
